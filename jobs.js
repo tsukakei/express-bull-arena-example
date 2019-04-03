9dbfb8e9b6d8bed115dfa131417808c56a8fe674
Keita Tsukamoto <keita.tsukamoto@yesod.co>
2019-04-03 11:38:34 +0900

    update job

--- jobs.js
+++ jobs.js
@@ -3,9 +3,17 @@ const Bull = require("bull");
 const router = express.Router();
 const queue = new Bull('Sample', 'redis://127.0.0.1:6379');
 queue.process(function(job, done) {
-  setInterval(function() {
-    console.log('Yor jon is executed!');
-    done();
+  console.log('start executing your job');
+  let rand = Math.floor(Math.random() * 3);
+  console.log(rand);
+  setTimeout(function() {
+    if (rand == 2) {
+      console.log('Success!');
+      done();
+    } else {
+      console.log('Failed...');
+      done(Error);
+    }
   }, 1000 * 5);
 });
 
